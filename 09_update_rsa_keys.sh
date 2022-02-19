10e612bd506478c86032a05460c55c169b06d5a8
mark <mark@example.com>
2022-02-19 15:44:54 +1100

    Update

--- 09_update_rsa_keys.sh
+++ 09_update_rsa_keys.sh
@@ -5,7 +5,7 @@ sudo chmod 700 /root/.ssh
 cp ./id_rsa /root/.ssh
 sudo chmod 600 /root/.ssh/id_rsa
 
-cp ./id_rsa /root/.ssh
+cp ./id_rsa.pub /root/.ssh
 sudo chmod 644 /root/.ssh/id_rsa.pub
 
 sudo chown -R root:root /root/.ssh
@@ -16,7 +16,7 @@ sudo chmod 700 /home/mark/.ssh
 cp ./id_rsa /home/mark/.ssh
 sudo chmod 600 /home/mark/.ssh/id_rsa
 
-cp ./id_rsa /home/mark/.ssh
+cp ./id_rsa.pub /home/mark/.ssh
 sudo chmod 644 /home/mark/.ssh/id_rsa.pub
 
 sudo chown -R mark:mark /root/.ssh
