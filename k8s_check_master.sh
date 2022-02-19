fa8e77ce2162f4aa254755e400b344566df15132
mark <mark@example.com>
2022-02-19 17:26:17 +1100

    Update

--- k8s_check_master.sh
+++ k8s_check_master.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 if [ "$HOSTNAME" = "k8smaster" ]; then
-    /root/ubuntu20_04_k8s_kvm/k8s_master1.sh
+    /root/ubuntu20_04_k8s_kvm/k8s_master1.sh && /root/ubuntu20_04_k8s_kvm/k8s_master2.sh 
 fi
--- k8s_worker.sh
+++ k8s_worker.sh
@@ -6,6 +6,7 @@ sudo mkdir -p /home/mark/.kube
 sudo cp /tmp/config /home/mark/.kube
 sudo chown -R mark:mark /home/mark/.kube
 
+chmod +x /tmp/join.sh
 /tmp/join.sh
 NODENAME=$(hostname -s)
 kubectl label node $(hostname -s) node-role.kubernetes.io/worker=worker-new
