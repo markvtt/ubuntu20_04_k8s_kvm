a3a2c931c062ad2381eb1781507424e1fcfe7d5a
mark <mark@example.com>
2022-02-19 13:17:58 +1100

    Update

--- 01_preqs.sh
+++ 01_preqs.sh
@@ -1,6 +1,4 @@
 echo -e "Ensure value not 0"
 egrep -c '(vmx|svm)' /proc/cpuinfo
 
-echo -e "Verify network devices (script is using wlp4s0) "
-nmcli con show
 
--- 03_check_kvm.sh
+++ 03_check_kvm.sh
@@ -1,7 +1,6 @@
-echo -e "Ensure:"
-echo -e "INFO: /dev/kvm exists"
-echo -e "KVM acceleration can be used"
+echo -e "Ensure kvm exists"
 kvm-ok
 
 echo -e "Ensure value is 1"
 cat /proc/sys/net/ipv4/ip_forward
+
